451c3bf7d9fb74753f0a92d2d39af99a5bb51254
Bert McLees <bert.mclees@gmail.com>
2016-10-15 17:25:30 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,15 @@
+// ScratchPad for Javascript
+
+console.log('test again');
+
+var xyz = "Able";
+
+function tommy(vary) {
+    var x = vary;
+    console.log(x);
+    xyz = 'bozo';
+}
+
+tommy('kill');
+console.log(xyz);
+
